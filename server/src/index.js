48fcb06cd524cd040dcec72f76efdde1cbe608ab
sudodot <haikhalroy@gmail.com>
2025-04-26 21:30:18 +0700

    fix: fix my dumbahh forgeting to put comma when creating defining mysql connection

--- server/src/index.js
+++ server/src/index.js
@@ -12,10 +12,10 @@ app.use(express.json());
 
 // database connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST
-  user: process.env.DB_USER
-  password: process.env.DB_PASSWORD
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
